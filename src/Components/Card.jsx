3be4bf26ea136e9c251d2b5253128399099e853e
amineakhrib <amine.akhrib@europcar.com>
2024-06-30 01:47:46 +0200

    ajout de nouvelles cartes

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,25 +1,31 @@
 import React from 'react';
-import PropTypes from 'prop-types'
-import ICalButton from './iCal.jsx'
+import PropTypes from 'prop-types';
+import ICalButton from './iCal.jsx';
 
 function Card({ img, alt, title, desc, descr, loc, jour, DDB, DDF }) {
+  // Vérifier si les props pour ICalButton sont valides
+  const isICalButtonValid = title && descr && loc && DDB && DDF;
+
+  // Vérifier si jour est vide ou non défini
+  const dateText = jour ? `${jour} Juillet 2024` : "choix de la date en cours";
+
   return (
     <div className="card">
-        <h3 className="card-title">{title}</h3>
-        <img src={img} alt={alt} className="card-image" />
-        <p>{jour} Juillet 2024</p>
-        <p className="card-description">{desc}</p>
+      <h3 className="card-title">{title}</h3>
+      <img src={img} alt={alt} className="card-image" />
+      <p>{dateText}</p>
+      <p className="card-description">{desc}</p>
 
-        <ICalButton title={title} descr={descr} loc={loc} DDB={DDB} DDF={DDF}/>
+      {isICalButtonValid && <ICalButton title={title} descr={descr} loc={loc} DDB={DDB} DDF={DDF} />}
     </div>
   );
 }
 
 Card.defaultProps ={
-    img: "https://via.placeholder.com/",
-    alt: "placeholder de substitution",
-    title: "en attente de modification",
-    desc: "en attente de modification"
-}
+  img: "https://via.placeholder.com/",
+  alt: "placeholder de substitution",
+  title: "en attente de modification",
+  desc: "en attente de modification"
+};
 
 export default Card;
